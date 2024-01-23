1320c7e3dcd6c5f9ae9c1b519d69373b7e7f529e
Lysdestic <github@lysdestic.com>
2024-01-23 13:44:08 -0800

    removed vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,7 @@
 import { defineConfig } from 'astro/config';
-import vercel from "@astrojs/vercel/serverless";
-
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: vercel(),
   integrations: [icon()]
 });
